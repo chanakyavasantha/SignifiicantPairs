e8c198c8519887cc0cdc1fba7887326d3478ca32
chana <chanakyavasantha@gmail.com>
2022-09-02 18:44:20 +0530

    02/09/22

--- main.cpp
+++ main.cpp
@@ -3,12 +3,21 @@ using namespace std;
 
 map<int,int> m;
 int ans = 0;
-void Search(int* b,int start,int end,float k,int j) {
-    int middle = (start + end) / 2;
+void Search(int* b,int start,int end,int k,int j) {
+    int middle = (start + end)/ 2;
     if (start <= end) {
         if (b[middle] <= k && m[b[middle]] < j) {
-            //cout<<b[middle]<<endl;
-            ans+= middle+1;
+            while(true) {
+                if (b[middle + 1] <= k) {
+                    middle += 1;
+                }
+                else{
+                    break;
+                }
+            }
+            //for (int i = 0; i <= middle; i++)
+                //cout << b[i] << " ";
+            ans += middle + 1;
             return;
         }
         else{
@@ -29,12 +38,12 @@ int main(){
     for(int i = 0;i<n;i++){
         m.insert(make_pair(a[i],i));
     }
-    for(int j = 0;j<n;j++){
-        float k = a[j]/2;
-        //cout<<"pairs: "<<endl;
-        //cout<<a[j]<<endl;
+    for(int j = 1;j<n;j++){
+        int k = a[j]/2;
+        //cout<<a[j];
+        //cout<<"->";
         Search(b,0,j,k,j);
+        //cout<<endl;
     }
     cout<<ans<<endl;
-
 }
